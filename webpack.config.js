6f915efb0949c538b3e3010b9ffaeaefe81990a6
William Cory <willcory10@gmail.com>
2018-04-10 13:25:30 -0700

    make react render

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,23 @@
 const path = require('path');
-const SRC_DIR = path.join(__dirname, './client');
-const DIST_DIR = path.join(__dirname, './client/dist');
+const SRC_DIR = path.join(__dirname, '/client/src');
+const DIST_DIR = path.join(__dirname, '/client/dist');
 
 module.exports = {
-  entry: `${DIST_DIR}/index.html`,
+  entry: `${SRC_DIR}/index.jsx`,
   output: {
     filename: 'bundle.js',
     path: DIST_DIR
   },
-  module: {
-    test: /\.jsx$/,
-    loader: 'babel-loader',
-    include: SRC_DIR,
-    query: {
-      presets: ['react', 'es2015']
-    }
+  module : {
+    loaders : [
+      {
+        test : /(\.jsx?)/,
+        include : SRC_DIR,
+        loader : 'babel-loader',      
+        query: {
+          presets: ['react', 'es2015']
+       }
+      }
+    ]
   }
 };
