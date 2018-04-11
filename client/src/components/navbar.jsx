3a36dd22d506b6d7c63077d58460b42af6ca7faf
wvha <Wvha17@gmail.com>
2018-04-11 15:20:57 -0700

    got react-bootstrap components to work

---
+++ client/src/components/navbar.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Navbar, NavItem, MenuItem, Nav, NavDropdown } from 'react-bootstrap';
+
+
+
+class NavbarComp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <Navbar collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="#brand">React-Bootstrap</a>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav>
+            <NavItem eventKey={1} href="#">
+              Link
+            </NavItem>
+            <NavItem eventKey={2} href="#">
+              Link
+            </NavItem>
+            <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+              <MenuItem eventKey={3.1}>Action</MenuItem>
+              <MenuItem eventKey={3.2}>Another action</MenuItem>
+              <MenuItem eventKey={3.3}>Something else here</MenuItem>
+              <MenuItem divider />
+              <MenuItem eventKey={3.3}>Separated link</MenuItem>
+            </NavDropdown>
+          </Nav>
+          <Nav pullRight>
+            <NavItem eventKey={1} href="#">
+              Link Right
+            </NavItem>
+            <NavItem eventKey={2} href="#">
+              Link Right
+            </NavItem>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+
+    );
+  }
+}
+
+export default NavbarComp;
