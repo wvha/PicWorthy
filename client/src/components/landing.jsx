f3e49d06b5587b328c636187a523e9778e57000f
Christina <christina.elaine.yuen@gmail.com>
2018-04-12 21:07:52 -0700

    Added jumbotron

--- client/src/components/landing.jsx
+++ client/src/components/landing.jsx
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
 import { Carousel, CarouselItem } from 'react-bootstrap';
+import { Jumbotron } from 'react-bootstrap';
 
 class Landing extends Component {
   constructor(props) {
     super(props);
   }
 
-
-
   render() {
     return (<div>
-      <Carousel  style={carouselStyle}>
+      <Carousel style={carouselStyle}>
         <Carousel.Item>
           <img style={imgStyle} src="https://images.pexels.com/photos/7653/pexels-photo.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" />
           <Carousel.Caption>
@@ -19,7 +18,7 @@ class Landing extends Component {
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <img style={imgStyle}src="https://www.californiabeaches.com/wp-content/uploads/2017/04/bigs-Lands-End-Beach-Labyrinth-with-Golden-Gate-Bridge-E1-Large-1000x610.jpg" />
+          <img style={imgStyle} src="https://www.californiabeaches.com/wp-content/uploads/2017/04/bigs-Lands-End-Beach-Labyrinth-with-Golden-Gate-Bridge-E1-Large-1000x610.jpg" />
           <Carousel.Caption>
             <h3>Lands End Labyrinth</h3>
             <p>Lands End Trail, San Francisco, CA</p>
@@ -32,20 +31,28 @@ class Landing extends Component {
             <p>Kerry Park, Seattle, WA</p>
           </Carousel.Caption>
         </Carousel.Item>
-      </Carousel>;
+      </Carousel>
+
+      <Jumbotron>
+        <h1>Hello, world!</h1>
+        <p>
+          This is a simple hero unit, a simple jumbotron-style component for calling
+          extra attention to featured content or information.
+        </p>
+      </Jumbotron>;
     </div>)
   }
 };
 
 const carouselStyle = {
   height: "500px",
-  width:"100%",
+  width: "100%",
   margin: "0px"
 }
 
 const imgStyle = {
-  width:'100%',
-  height:'500px',
+  width: '100%',
+  height: '500px',
   objectFit: 'cover'
 }
 
