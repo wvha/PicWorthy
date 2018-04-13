64ec69bc494a0528ed5e1ce27cd78501cd9ae03f
William Cory <willcory10@gmail.com>
2018-04-12 21:51:02 -0700

    trying to merge with master

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -5,16 +5,8 @@ import { Navbar } from 'react-bootstrap';
 import { BrowserRouter, Route, Switch} from 'react-router-dom';
 import Login from './components/login.jsx';
 import Signup from './components/signup.jsx';
-<<<<<<< HEAD
-import Accept from './components/imjur.jsx';
-=======
 import Landing from './components/landing.jsx'
->>>>>>> 846bc7ae6ed4dae635c7387825f5dc57dbce9158
 // import 'bootstrap/dist/css/bootstrap.css';
-import Dropzone from 'react-dropzone';
-import axios from 'axios';
-import base64 from 'base-64';
-import imgurUploader from 'imgur-uploader';
 
 // I am testing the react router here this can be deleted
 const fakeHomePageComponent = (props) => <div>this is a fake home page</div>;
@@ -22,76 +14,14 @@ const fakeLoginComponent = (props) => <div>fake login component</div>;
 const fakeSignupComponent = (props) => <div>fake signup compoonent</div>;
 
 class App extends Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props);
     this.state = {
-      accepted: [],
-      rejected: []
-    }
-  }
-
-
-  onDrop(img) {
-    const imgurRequest = (base64img) => {
-      imgurUploader(img[0]);
-      // axios({
-      //   method: 'post',
-      //   url: 'https://api.imgur.com/3/image',
-      //   headers: {Authorization: "Client-ID 3f9b22888755abe"},
-      //   data: {
-      //       image: base64img
-      //   }
-
-      // })
-      // .then(function(response) {
-      //     console.log(response);
-      // })
-      // .catch(function(err) {
-      //     console.log(err);
-      // })
-    }
-    var reader = new FileReader();
-    reader.readAsArrayBuffer(img[0]);
-    reader.onload = function () {
-      console.log(reader.result);
-      imgurRequest(reader.result);
     };
-    reader.onerror = function (error) {
-       console.log('Error: ', error);
-     };
-
   }
 
   render() {
     return (
-<<<<<<< HEAD
-      <section>
-        <div className="dropzone">
-          <Dropzone
-            accept="image/jpeg, image/png"
-            onDrop={(this.onDrop.bind(this))}
-          >
-            <p>Try dropping some files here, or click to select files to upload.</p>
-            <p>Only *.jpeg and *.png images will be accepted</p>
-          </Dropzone>
-        </div>
-        <aside>
-          <h4>Accepted files</h4>
-          <ul>
-            {
-              this.state.accepted.map(f => <li key={f.name}>{f.name} - {f.size} bytes</li>)
-            }
-          </ul>
-          <h4>Rejected files</h4>
-          <ul>
-            {
-              this.state.rejected.map(f => <li key={f.name}>{f.name} - {f.size} bytes</li>)
-            }
-          </ul>
-        </aside>
-        <button type="submit">upload</button>
-      </section>
-=======
       <div>
         <NavbarComp />
         <Switch>
@@ -100,13 +30,12 @@ class App extends Component {
           <Route path='/signup' component={Signup} />
         </Switch>
       </div>
->>>>>>> 846bc7ae6ed4dae635c7387825f5dc57dbce9158
     );
   }
 }
 
 ReactDOM.render((
-
+  <BrowserRouter>
     <App />
-
+  </BrowserRouter>
 ), document.getElementById('app'));
