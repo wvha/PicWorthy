6a1f9bf9004d9a251da3efe8a641495b78b59a19
Christina <christina.elaine.yuen@gmail.com>
2018-04-17 12:22:17 -0700

    Working card feature

--- client/src/components/card.jsx
+++ client/src/components/card.jsx
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
 
-class Card extends Component {
-  constructor(props) {
-    super(props)
-  }
+const Card = ({src, location}) => (
+  <span style={spanStyle}>
+    <img src={src} style={imgStyle} />
+    <br/> <br/>
+    <div style={locationStyle}>{location}</div>
+  </span>
+)
 
-  render() {
-    return (
-      <div>
-      </div>
-    )
-  }
+const spanStyle ={
+  display: "inline-block",
+  padding: "15px 15px 70px 15px",
+  height: "350px",
+  width: "250px",
+  border: "1px solid black",  
+  margin: "5px",
+  borderRadius: "3px"
 }
+
+const imgStyle = {
+  border: "1px solid #555",
+  height: "100%",
+  width: "100%",
+  objectFit: "cover"
+}
+
+const locationStyle = {
+  textTransform: "capitalize",
+  fontFamily: "Comic Sans MS, Arial, sans-serif",
+  fontWeight: "bold",
+  fontSize: "large"
+}
+
+export default Card;
--- client/src/components/row.jsx
+++ client/src/components/row.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import FontAwesome from 'react-fontawesome';
 import FaIconPack, {FaChevronRight, FaChevronLeft} from 'react-icons/lib/fa';
+import Card from './card.jsx';
 
 class Row extends Component {
   constructor(props) {
     super(props);
-    this.displayAmount = 5;
+    this.displayAmount = 4;
     this.startIndex = 0;
     this.state = {
       picsDisplay: picsDb.slice(0, this.displayAmount)
@@ -36,10 +37,13 @@ class Row extends Component {
 
   render() {
     return (
-      <div style={{ width: "1190px", margin: "auto"}}>
+      <div style={{textAlign: "center"}}>
         <FaChevronLeft onClick={this.handlePrevious} style={chevronStyle}/>
-        {this.state.picsDisplay.map((pic, i) => {
+        {/* {this.state.picsDisplay.map((pic, i) => {
           return <img src={pic} key={i} style={{padding:"10px"}}/>
+        })} */}
+        {this.state.picsDisplay.map((pic, i) => {
+          return <Card src={pic.src} key={i} location={pic.location}/>
         })}
         <FaChevronRight onClick={this.handleNext} style={chevronStyle}/>
         <br/>
@@ -50,22 +54,26 @@ class Row extends Component {
 }
 
 const chevronStyle = {
-  paddingRight:"45px", 
-  paddingTop: "180px", 
-  paddingBottom: "220px"
+  verticalAlign: "top",
+  paddingRight: "45px",
+  paddingTop: "170px",
+  paddingBottom: "180px",
+  display: "inline-block",
+  margin: "5px"
 }
 
 const picsDb = [
-  'http://lorempixel.com/output/cats-h-c-200-400-1.jpg',
-  'http://lorempixel.com/output/cats-h-g-200-400-7.jpg',
-  'http://lorempixel.com/output/cats-h-c-200-400-2.jpg',
-  'http://lorempixel.com/output/cats-h-g-200-400-6.jpg',
-  'http://lorempixel.com/output/cats-h-c-200-400-3.jpg',
-  'http://lorempixel.com/output/cats-h-g-200-400-1.jpg',
-  'http://lorempixel.com/output/cats-h-c-200-400-4.jpg',
-  'http://lorempixel.com/output/cats-h-g-200-400-3.jpg',
-  'http://lorempixel.com/output/cats-h-c-200-400-5.jpg',
-  'http://lorempixel.com/output/animals-h-g-200-400-5.jpg',
+  {src: 'http://lorempixel.com/output/cats-h-c-200-400-1.jpg', location: 'Armsterdam'},
+  {src: 'http://lorempixel.com/output/cats-h-g-200-400-7.jpg', location: 'Belgium'},
+  {src: 'http://lorempixel.com/output/cats-h-c-200-400-2.jpg', location: 'China'},
+  {src: 'http://lorempixel.com/output/cats-h-g-200-400-6.jpg', location: 'Denmark'},
+  {src: 'http://lorempixel.com/output/cats-h-c-200-400-3.jpg', location: 'Ethiopia'},
+  {src: 'http://lorempixel.com/output/cats-h-g-200-400-1.jpg', location: 'France'},
+  {src: 'http://lorempixel.com/output/cats-h-c-200-400-4.jpg', location: 'Germany'},
+  {src: 'http://lorempixel.com/output/cats-h-g-200-400-3.jpg', location:'Italy'},
+  {src: 'http://lorempixel.com/output/cats-h-c-200-400-5.jpg', location:'Japan'},
+  {src: 'http://lorempixel.com/output/animals-h-g-200-400-5.jpg', location: 'Korea'},
 ];
+
 
 export default Row;
