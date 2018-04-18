7acc7879dbf2db6a87910f1e9e406c968fccdeb3
William Cory <willcory10@gmail.com>
2018-04-17 17:25:59 -0700

    add zoom feature

--- client/src/components/worthymap.jsx
+++ client/src/components/worthymap.jsx
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
-import { compose, withProps, withHandlers } from 'recompose';
+import _ from 'lodash';
+import { compose, withProps, withHandlers, lifecycle } from 'recompose';
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
 import { Grid, Row } from 'react-bootstrap';
 import { MarkerClusterer }  from 'react-google-maps/lib/components/addons/MarkerClusterer';
+const { SearchBox } = require("react-google-maps/lib/components/places/SearchBox");
+
 
 // https://tomchentw.github.io/react-google-maps/#markerclusterer
 const ClusteredMap = compose(
@@ -19,6 +22,57 @@ const ClusteredMap = compose(
       console.log(clickedMarkers)
     },
   }),
+  lifecycle({
+    componentWillMount() {
+      const refs = {}
+
+      this.setState({
+        zoom: 5,
+        bounds: null,
+        center: {
+          lat: 41.9, lng: -87.624
+        },
+        onMapMounted: ref => {
+          refs.map = ref;
+        },
+        onBoundsChanged: () => {
+          this.setState({
+            bounds: refs.map.getBounds(),
+            center: refs.map.getCenter(),
+          })
+        },
+        onSearchBoxMounted: ref => {
+          refs.searchBox = ref;
+        },
+        onPlacesChanged: () => {
+          const places = refs.searchBox.getPlaces();
+          const bounds = new google.maps.LatLngBounds();
+
+          places.forEach(place => {
+            if (place.geometry.viewport) {
+              bounds.union(place.geometry.viewport)
+            } else {
+              bounds.extend(place.geometry.location)
+            }
+          });
+          const nextMarkers = places.map(place => ({
+            position: place.geometry.location,
+          }));
+          const nextCenter = _.get(nextMarkers, '0.position', this.state.center);
+
+          // hacky way of making sure the zoom updates even if the user changed the zoom
+          this.setState({
+            zoom: 1
+          })
+          this.setState({
+            center: nextCenter,
+            zoom: 11
+          });
+          console.log(this.state);
+        },
+      })
+    },
+  }),
   withScriptjs,
   withGoogleMap
 ) ((props) => {
@@ -32,8 +86,10 @@ const ClusteredMap = compose(
 
   return (
     <GoogleMap
-      defaultZoom={ props.zoom }
-      defaultCenter={ props.center }
+      ref={props.onMapMounted}
+      zoom={ props.zoom }
+      center={ props.center }
+      onBoundsChanged={ props.onBoundsChanged }
       onClick={ props.clickMap }
     >
       <MarkerClusterer
@@ -44,6 +100,30 @@ const ClusteredMap = compose(
       >
         { allMarkers }
       </MarkerClusterer>
+      <SearchBox
+        ref={props.onSearchBoxMounted}
+        bounds={props.bounds}
+        controlPosition={google.maps.ControlPosition.TOP_LEFT}
+        onPlacesChanged={props.onPlacesChanged}
+      >
+        <input
+          type="text"
+          placeholder="Customized your placeholder"
+          style={{
+            boxSizing: `border-box`,
+            border: `1px solid transparent`,
+            width: `240px`,
+            height: `32px`,
+            marginTop: `27px`,
+            padding: `0 12px`,
+            borderRadius: `3px`,
+            boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+            fontSize: `14px`,
+            outline: `none`,
+            textOverflow: `ellipses`,
+          }}
+        />
+      </SearchBox>
     </GoogleMap>
   );
 });
