6372a519911d2b9a57820b839785b46314fb5118
William Cory <willcory10@gmail.com>
2018-04-17 17:59:16 -0700

    add search feature

--- client/src/components/worthymap.jsx
+++ client/src/components/worthymap.jsx
@@ -108,19 +108,21 @@ const ClusteredMap = compose(
       >
         <input
           type="text"
-          placeholder="Customized your placeholder"
+          placeholder="Search"
           style={{
             boxSizing: `border-box`,
             border: `1px solid transparent`,
             width: `240px`,
             height: `32px`,
-            marginTop: `27px`,
+            margin: 'auto auto auto auto',
+            marginTop: `10px`,
             padding: `0 12px`,
             borderRadius: `3px`,
             boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
             fontSize: `14px`,
             outline: `none`,
             textOverflow: `ellipses`,
+            left: '500px'
           }}
         />
       </SearchBox>
