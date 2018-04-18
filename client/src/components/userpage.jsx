61d2690ba161677f2ade175574dbf9949af46652
wvha <Wvha17@gmail.com>
2018-04-18 11:49:51 -0700

    created userpage.jsx file

---
+++ client/src/components/userpage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class UserPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <h1>Hello this is the user page.</h1>
+    )
+  }
+}
+
+export default UserPage;
