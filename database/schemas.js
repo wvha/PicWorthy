4cc9ec76a05c4803d7d241073c33b06ec4e6d047
wvha <Wvha17@gmail.com>
2018-04-18 17:09:52 -0700

    added potential schema table for pics and likes

--- database/schemas.js
+++ database/schemas.js
@@ -4,8 +4,18 @@ const userSchema = mongoose.Schema({
   firstName: String,
   lastName: String,
   username: String,
-  password: String
+  password: String,
+  photos: [{ type: Schema.Types.ObjectId, ref: 'photos' }],
+  likes: [{ type: Schema.Types.ObjectId, ref: 'photos'}],
+});
+
+const mockPhotoSchema = mongoose.Schema({
+  url: String,
+  likedBy: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  user: { type: Schema.Types.ObjectId, ref: 'User' }
 });
 
 module.exports.userSchema = userSchema;
+module.exports.mockPhotoSchema = mockPhotoSchema;
 
+// http://mongoosejs.com/docs/populate.html
