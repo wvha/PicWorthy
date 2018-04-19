64ef9f030512e353c927fb9c6bb6f259c537189a
wvha <Wvha17@gmail.com>
2018-04-19 15:52:42 -0700

    adding axios to userpage

--- client/src/components/userpage.jsx
+++ client/src/components/userpage.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import Row from './row.jsx';
 
+
 class Userpage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
+
+  componentDidMount() {
+    axios.get('/user')
+  }
+
+// need function to call users pics and set state 
+
+// need function to call users likes and set second state
 
   render() {
     return (
