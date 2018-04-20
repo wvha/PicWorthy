a861876b064f918d794a026e24b85c311530c036
wvha <Wvha17@gmail.com>
2018-04-19 20:16:17 -0700

    will work on this later

--- client/src/components/likes.jsx
+++ client/src/components/likes.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import Row from './row.jsx';
-
+import axios from 'axios';
 
 class Likes extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      likes: {}
+    };
   }
 
-// need function to call users pics and set state 
-
 // need function to call users likes and set second state
+  componentDidMount() {
+    axios.get('/api/likes', {user: this.props.userData.username})
+      .then((result) => {
+        console.log('this.props.userdata.username: ', this.props.userData.username);
+        console.log('success in likes//component did mount: ', result);
+        this.setState({
+          likes: result.data
+        });
+      });
+  }
 
   render() {
     return (
